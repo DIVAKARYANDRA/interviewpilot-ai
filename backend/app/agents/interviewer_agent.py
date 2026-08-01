2da424095e5258af9b181104863554a0be6e4afc
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-08-01 16:13:15 +0530

    error fixed

--- backend/app/agents/interviewer_agent.py
+++ backend/app/agents/interviewer_agent.py
@@ -1,9 +1,20 @@
 from app.agents.base_agent import BaseAgent
 
-from app.prompts.system_prompt import SYSTEM_PROMPT
 from app.prompts.prompt_builder import PromptBuilder
+
 from app.services.gemini_service import GeminiService
 
-prompt = PromptBuilder.build_interview_prompt(state)
 
-question = GeminiService.generate(prompt)
+class InterviewerAgent(BaseAgent):
+
+    def execute(self, state):
+
+        prompt = PromptBuilder.build_interview_prompt(state)
+
+        question = GeminiService.generate(prompt)
+
+        state["current_question"] = question
+
+        state["previous_questions"].append(question)
+
+        return state
