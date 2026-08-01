684ef38018f79386d609bef1fb31ddc677ff254d
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-08-01 19:23:49 +0530

    useinterview import added

--- frontend/src/pages/InterviewSetup/InterviewSetupPage.tsx
+++ frontend/src/pages/InterviewSetup/InterviewSetupPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { useInterview } from "../../context/InterviewContext";
 import MainLayout from "../../layouts/MainLayout";
 import { startInterview } from "../../services/interviewService";
 
