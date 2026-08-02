28fce12644d8f839dd96e1ac2b71c0a0afd702fa
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-08-02 13:38:00 +0530

    auth.ts updated

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -1,8 +1,19 @@
 export interface RegisterRequest {
-  name: string;
-  email: string;
-  password: string;
-  experience: number;
+
+    name:string;
+
+    email:string;
+
+    password:string;
+
+    experience:number;
+
+    current_company:string;
+
+    target_company:string;
+
+    target_role:string;
+
 }
 
 export interface LoginRequest {
