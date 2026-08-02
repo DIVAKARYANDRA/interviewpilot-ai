8aa1a5f2f8305abd8dea575bc9f2704b19a08278
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-08-02 22:50:49 +0530

    api changed

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,10 +1,47 @@
+export default api;
+
 import axios from "axios";
 
+import { getToken } from "../utils/token";
+
+
 const api = axios.create({
-  baseURL: "https://interviewpilot-ai-2cmo.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
+
+    baseURL:
+    "https://interviewpilot-ai-2cmo.onrender.com"
+
 });
+
+
+
+api.interceptors.request.use(
+
+    (config)=>{
+
+
+        const token = getToken();
+
+
+        if(token){
+
+            config.headers.Authorization =
+                `Bearer ${token}`;
+
+        }
+
+
+        return config;
+
+    },
+
+    (error)=>{
+
+        return Promise.reject(error);
+
+    }
+
+);
+
+
 
 export default api;
