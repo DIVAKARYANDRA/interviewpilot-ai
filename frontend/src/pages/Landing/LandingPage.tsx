0268257aa3c1264ac2418c65fdbc10e6356853ed
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-08-04 21:51:18 +0530

    statistics path change

--- frontend/src/pages/Landing/LandingPage.tsx
+++ frontend/src/pages/Landing/LandingPage.tsx
@@ -3,7 +3,7 @@ import LiveDemo from "../../components/landing/LiveDemo/LiveDemo";
 import Hero from "../../components/landing/Hero/Hero";
 import Features from "../../components/landing/Features/Features";
 import HowItWorks from "../../components/landing/HowItWorks/HowItWorks";
-import Statistics from "../components/landing/Statistics/Statistics";
+import Statistics from "../../components/landing/Statistics/Statistics";
 
 export default function LandingPage() {
   return (
